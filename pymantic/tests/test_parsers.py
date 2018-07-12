27159d3075b7be0ca65b36d677f503f37db0aa54
Nicholas Pilon <npilon@lexmachina.com>
2018-07-12 11:56:17 -0700

    Don't use keywords for .parse()

--- pymantic/tests/test_parsers.py
+++ pymantic/tests/test_parsers.py
@@ -100,6 +100,6 @@ def test_parse_turtle_example_1():
     ex:homePage <http://purl.org/net/dajobe/>
   ] ."""
     g = Graph()
-    turtle_parser.parse(data=ttl, sink=g)
+    turtle_parser.parse(ttl, g)
     assert len(g) == 4
 

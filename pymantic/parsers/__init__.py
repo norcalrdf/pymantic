892515d63eddec18adec1176eda24e7c9af7303d
Nicholas Pilon <npilon@lexmachina.com>
2018-07-12 11:39:39 -0700

    Swap to lark turtle parser

--- pymantic/parsers/__init__.py
+++ pymantic/parsers/__init__.py
@@ -2,9 +2,9 @@ __all__ = ['ntriples_parser', 'nquads_parser', 'turtle_parser', 'jsonld_parser']
 
 from .lepl import (
     nquads_parser,
-    turtle_parser,
     jsonld_parser,
 )
 from .lark import (
     ntriples_parser,
+    turtle_parser,
 )

584b934bdd6bc47581aeca762cf37a70e0c7a9d7
Gavin Carothers <gavin@carothers.name>
2023-07-13 08:35:52 -0700

    Apply black to all src files

--- src/pymantic/parsers/lark/__init__.py
+++ src/pymantic/parsers/lark/__init__.py
@@ -3,7 +3,7 @@ from . import turtle as turtle_parser
 from .nquads import nquads_parser
 
 __all__ = [
-    'ntriples_parser',
-    'turtle_parser',
-    'nquads_parser',
+    "ntriples_parser",
+    "turtle_parser",
+    "nquads_parser",
 ]
